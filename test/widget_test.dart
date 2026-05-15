faa0a94e92028ebfdb24fc5fcdd15289ffda61c5
UmarEngginering <silfha342@gmail.com>
2026-05-15 12:33:18 +0700

    inisialisasi struktur utama

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:nur_kitab/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const NurKitabApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
